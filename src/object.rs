6c72426b6a30f1b54c466534eea6a3a58c48f672
Evan Day <evan.day@gmail.com>
2023-02-13 18:25:23 -0800

    Object functions

--- src/object.rs
+++ src/object.rs
@@ -1,190 +1,424 @@
-use std::fmt;
+/// Read a word from a memory map
+///
+/// # Arguments:
+///
+/// * `m` - Memory map
+/// * `a` - address
+fn word_value(m: &Vec<u8>, a: usize) -> u16 {
+    let hb = m[a];
+    let lb = m[a + 1];
 
-use crate::text::as_text;
-
-pub struct Property {
-    address: usize,
-    number: u8,
-    size: usize,
-    data: Vec<u8>
+    (((hb as u16) << 8) & 0xFF00) + ((lb as u16) & 0xFF)
 }
 
-impl fmt::Display for Property {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        writeln!(f, "\tNumber: {}", self.number)?;
-        writeln!(f, "\tSize: {}", self.size)?;
-        write!(f, "\tData: [")?;
-        for b in &self.data {
-            write!(f, " ${:02x}", b)?;
-        }
-        write!(f, " ]")
+fn set_word(m: &mut Vec<u8>, a: usize, v: u16) {
+    let hb = (v >> 8) as u8 & 0xFF;
+    let lb = (v & 0xFF) as u8;
+    m[a] = hb;
+    m[a+1] = lb;
+}
+
+/// Return the object table byte address stored at $0010 in the memory map
+///
+/// # Arguments:
+///
+/// * `m` - Memory map
+fn object_table_address(m: &Vec<u8>) -> usize {
+    word_value(m, 10) as usize
+}
+
+/// Return the default value for a property
+///
+/// # Arguments:
+///
+/// * `m` - Memory map
+/// * `p` - Property number
+fn default_property(m: &Vec<u8>, p: u8) -> Vec<u8> {
+    let ota = object_table_address(m);
+    let pa = ota + (p as usize * 2);
+    let mut v = Vec::new();
+    v.push(m[pa]);
+    v.push(m[pa + 1]);
+    v
+}
+
+/// Calculate the address of an object in the memory map
+///
+/// # Arguments:
+///
+/// * `m` - Memory map
+/// * `v` - ZMachine version (1-8)
+/// * `o` - Object number (1 - ...)
+pub fn object_address(m: &Vec<u8>, v: u8, o: usize) -> usize {
+    match v {
+        1 | 2 | 3 => object_table_address(m) + 62 + (9 * (o - 1)),
+        4 | 5 | 6 | 7 | 8 => object_table_address(m) + 126 + (14 * (o - 1)),
+        // TODO: Error
+        _ => 0,
     }
 }
-impl Property {
-    fn from_address(m: &Vec<u8>, v: u8, a: usize) -> Property {
-        match v {
-            1 | 2 | 3 => {
-                let size = (m[a] / 32) as usize + 1;
-                let num = m[a] & 0x1F;
-                let data = m.as_slice()[a+1..a+1+size].to_vec();
-                Property {
-                    address: a,
-                    number: num,
-                    size: size,
-                    data: data
-                }
-            },
-            _ => Property {
-                address: 0,
-                number: 0,
-                size: 0,
-                data: Vec::new()
+
+/// Get an attribute value, 1 = set, 0 = clear
+///
+/// # Arguments:
+///
+/// * `m` - Memory map
+/// * `v` - ZMachine verison (1-8)
+/// * `o` - Object number
+/// * `a` - Attribute number
+pub fn attribute(m: &Vec<u8>, v: u8, o: usize, a: usize) -> u8 {
+    let oa = object_address(m, v, o);
+    match v {
+        1 | 2 | 3 => {
+            if a < 32 {
+                (m[oa + (a / 8)] >> (7 - (a % 8))) & 1
+            } else {
+                0
             }
         }
+        4 | 5 | 6 | 7 | 8 => {
+            if a < 48 {
+                let byte = m[oa + (a / 8)];
+                (byte >> (7 - (a % 8))) & 0x1
+            } else {
+                0
+            }
+        }
+        _ => 0,
     }
 }
 
-pub struct Object {
-    address: usize,
-    attributes: Vec<u8>,
-    parent: usize,
-    sibling: usize,
-    child: usize,
-    property_table_address: usize,
-    short_name: String,
-    properties: Vec<Property>
-}
-
-impl fmt::Display for Object {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        writeln!(f, "\tAddress: ${:x}", self.address)?;
-        for i in 0..self.attributes.len() {
-            writeln!(f, "\tAttribute {}: {}", i, self.attributes[i])?;
+/// Set an attribute
+///
+/// # Arguments:
+///
+/// * `m` - Memory map
+/// * `v` - ZMachine verison (1-8)
+/// * `o` - Object number
+/// * `a` - Attribute number
+pub fn set_attribute(m: &mut Vec<u8>, v: u8, o: usize, a: usize) {
+    let oa = object_address(m, v, o);
+    let mask: u8 = 1 << 7 - (a % 8);
+    match v {
+        1 | 2 | 3 => {
+            if a < 32 {
+                m[oa + (a / 8)] = m[oa + (a / 8)] | mask
+            }
         }
-        writeln!(f, "\tParent: {}", self.parent)?;
-        writeln!(f, "\tSibling: {}", self.sibling)?;
-        writeln!(f, "\tChild: {}", self.child)?;
-        writeln!(f, "\tProperty table: ${:x}", self.property_table_address)?;
-        writeln!(f, "\tShort name: {}", self.short_name)?;
-        writeln!(f, "\tProperties:")?;
-        for p in &self.properties {
-            writeln!(f, "{}", p)?;
+        4 | 5 | 6 | 7 | 8 => {
+            if a < 48 {
+                m[oa + (a / 8)] = m[oa + (a / 8)] | mask
+            }
         }
-        Ok(())
+        _ => {}
     }
 }
 
-impl Object {
-    fn from_addr(m: &Vec<u8>, v: u8, a: usize) -> Object {
-        let mut attributes = Vec::new();
-        let mut parent = 0;
-        let mut sibling = 0;
-        let mut child = 0;
-        let mut prop_addr = 0;
-        let mut props = Vec::new();
-        let mut short_name = String::new();
-        
+/// Clear an attribute
+///
+/// # Arguments:
+///
+/// * `m` - Memory map
+/// * `v` - ZMachine verison (1-8)
+/// * `o` - Object number
+/// * `a` - Attribute number
+pub fn clear_attribute(m: &mut Vec<u8>, v: u8, o: usize, a: usize) {
+    let oa = object_address(m, v, o);
+    let mask: u8 = 1 << 7 - (a % 8);
+    match v {
+        1 | 2 | 3 => {
+            if a < 32 {
+                m[oa + (a / 8)] = m[oa + (a / 8)] & !mask
+            }
+        }
+        4 | 5 | 6 | 7 | 8 => {
+            if a < 48 {
+                m[oa + (a / 8)] = m[oa + (a / 8)] & !mask
+            }
+        }
+        _ => {}
+    }
+}
+
+/// Return the parent object
+///
+/// # Arguments:
+///
+/// * `m` - Memory map
+/// * `v` - ZMachine verison (1-8)
+/// * `o` - Object number
+pub fn parent(m: &Vec<u8>, v: u8, o: usize) -> usize {
+    let oa = object_address(m, v, o);
+
+    match v {
+        1 | 2 | 3 => m[oa + 4] as usize,
+        4 | 5 | 6 | 7 | 8 => word_value(m, oa + 6) as usize,
+        _ => 0,
+    }
+}
+
+/// Set the parent object of an object.  This method does not update other objects
+/// in the object tree (old parent, siblings).  Improper use can very easily result
+/// in an invalid object tree state.
+/// 
+/// # Arguments:
+/// 
+/// * `m` - Memory map
+/// * `v` - Version (1-8)
+/// * `o` - Object number to update
+/// * `p` - Parent object number
+pub fn set_parent(m: &mut Vec<u8>, v: u8, o: usize, p: usize) {
+    let oa = object_address(m, v, o);
+
+    match v {
+        1 | 2 | 3 => m[oa + 4] = p as u8,
+        4 | 5 | 6 | 7 | 8 => set_word(m, oa + 6, p as u16),
+        _ => {}
+    }
+}
+
+/// Return the first sibling object
+///
+/// # Arguments:
+///
+/// * `m` - Memory map
+/// * `v` - ZMachine verison (1-8)
+/// * `o` - Object number
+pub fn sibling(m: &Vec<u8>, v: u8, o: usize) -> usize {
+    let oa = object_address(m, v, o);
+
+    match v {
+        1 | 2 | 3 => m[oa + 5] as usize,
+        4 | 5 | 6 | 7 | 8 => word_value(m, oa + 8) as usize,
+        _ => 0,
+    }
+}
+
+/// Set the first sibling of an object.  This method does not update other objects
+/// in the object tree.  Improper use can very easily result in an invalid object 
+/// tree state.
+/// 
+/// # Arguments:
+/// 
+/// * `m` - Memory map
+/// * `v` - Version (1-8)
+/// * `o` - Object number to update
+/// * `s` - Sibling object number
+pub fn set_sibling(m: &mut Vec<u8>, v: u8, o: usize, s: usize) {
+    let oa = object_address(m, v, o);
+
+    match v {
+        1 | 2 | 3 => m[oa + 5] = s as u8,
+        4 | 5 | 6 | 7 | 8 => set_word(m, oa + 8, s as u16),
+        _ => {}
+    }
+}
+
+/// Return the child object
+///
+/// # Arguments:
+///
+/// * `m` - Memory map
+/// * `v` - ZMachine verison (1-8)
+/// * `o` - Object number
+pub fn child(m: &Vec<u8>, v: u8, o: usize) -> usize {
+    let oa = object_address(m, v, o);
+
+    match v {
+        1 | 2 | 3 => m[oa + 6] as usize,
+        4 | 5 | 6 | 7 | 8 => word_value(m, oa + 10) as usize,
+        _ => 0,
+    }
+}
+
+/// Set the first child of an object.  This method does not update other objects
+/// in the object tree.  Improper use can very easily result in an invalid object 
+/// tree state.
+/// 
+/// # Arguments:
+/// 
+/// * `m` - Memory map
+/// * `v` - Version (1-8)
+/// * `o` - Object number to update
+/// * `c` - Child object number
+pub fn set_child(m: &mut Vec<u8>, v: u8, o: usize, c: usize) {
+    let oa = object_address(m, v, o);
+
+    match v {
+        1 | 2 | 3 => m[oa + 6] = c as u8,
+        4 | 5 | 6 | 7 | 8 => set_word(m, oa + 10, c as u16),
+        _ => {}
+    }
+}
+/// Get the address of the property table for an object
+///
+/// # Arguments:
+///
+/// * `m` - Memory map
+/// * `v` - Version (1-8)
+/// * `o` - Object number
+pub fn property_table_address(m: &Vec<u8>, v: u8, o: usize) -> usize {
+    let oa = object_address(m, v, o);
+    match v {
+        1 | 2 | 3 => word_value(m, oa + 7) as usize,
+        4 | 5 | 6 | 7 | 8 => word_value(m, oa + 12) as usize,
+        _ => 0,
+    }
+}
+
+/// Get an object's short name as a vector of ZSCII words
+///
+/// # Arguments:
+///
+/// * `m` - Memory map
+/// * `v` - Version (1-8)
+/// * `o` - Object number
+pub fn short_name(m: &Vec<u8>, v: u8, o: usize) -> Vec<u16> {
+    let pt = property_table_address(m, v, o);
+    let hs = m[pt] as usize;
+    let mut r = Vec::new();
+    for i in 0..hs {
+        r.push(word_value(m, pt + 1 + (i * 2)))
+    }
+    r
+}
+
+/// Check if a property is set for an object
+/// 
+/// # Arguments:
+/// 
+/// * `m` - Memory map
+/// * `v` - Version (1-8)
+/// * `o` - Object number
+/// * `p` - Property number
+pub fn has_property(m: &Vec<u8>, v: u8, o: usize, p: u8) -> bool {
+    let pt = property_table_address(m, v, o);
+    let hs = m[pt] as usize;
+    let mut pa = pt + 1 + (hs * 2);
+    let mut r = -1;
+
+    while r == -1 {
         match v {
+            // V1-3:
+            //  * Size/number encoded in a single byte
             1 | 2 | 3 => {
-                /* Attributes are stored as:
-                   0:  byte 0, bit 7
-                   7:  byte 0, bit 0
-                   8:  byte 1, bit 7
-                   15: byte 1, bit 0
-                   16: byte 2, bit 7
-                   23: byte 2, bit 0
-                   24: byte 3, bit 0
-                   31: byte 3, bit 7 */
-                for i in (1..=4).rev() {
-                    for j in (1..=8).rev() {
-                        let b = m[a + 4 - i];
-                        let attr = b >> (j - 1) & 0x1;
-                        attributes.push(attr);
-                    }
-                }
-                parent = m[a+4];
-                sibling = m[a+5];
-                child = m[a+6];
-                prop_addr = word_value(m, a + 7) as usize;
-                let ph_s = m[prop_addr] as usize;
-                short_name = as_text(m, prop_addr + 1);
-
-                let mut go = true;
-                let mut pa = prop_addr + (ph_s * 2) + 1;
-                while go {
-                    let p = Property::from_address(m, v, pa);
-                    if p.number > 0 {
-                        pa = pa + p.size + 1;
-                        props.push(p);
+                let sb = m[pa];
+                if sb == 0 {
+                    r = 0
+                } else {
+                    let n = sb & 0x1F;
+                    let s = (sb as usize / 32) + 1;
+                    if n == p {
+                        r = 1
+                    } else if n < p {
+                        r = 0
                     } else {
-                        go = false;
+                        pa = pa + 1 + s;
                     }
                 }
-                
+            },
+            // V4+:
+            //  * Size/number encoded in a single byte -or-
+            //  * Number in first byte, size in second
+            4 | 5 | 6 | 7 | 8 => {
+                let mut pd = pa + 1;
+                let sb = m[pa];
+                if sb == 0 {
+                    r = 0
+                } else {
+                    let n = sb & 0x3F;
+                    let s = {
+                        if sb & 0x80 == 0x80 {
+                            pd = pd + 1;
+                            m[pa + 1] as usize & 0x3F
+                        } else {
+                            if sb & 0x40 == 0x40 {
+                                2
+                            } else {
+                                1
+                            }
+                        }
+                    };
+
+                    if n == p {
+                        r = 1
+                    } else if n < p {
+                        r = 0
+                    } else {
+                        pa = pd + s;
+                    }
+                }
             },
             _ => {}
         }
-        Object {
-            address: a,
-            attributes: attributes,
-            parent: parent as usize,
-            sibling: sibling as usize,
-            child: child as usize,
-            property_table_address: prop_addr,
-            short_name: short_name,
-            properties: props,
+    }
+    r == 1
+}
+
+/// Get a property value for an object as a vector of bytes.  If the property is not
+/// set for the object itself, the default property value is returned.
+///
+/// # Arguments:
+///
+/// * `m` - Memory map
+/// * `v` - Version (1-8)
+/// * `o` - Object number
+/// * `p` - Property number
+pub fn property(m: &Vec<u8>, v: u8, o: usize, p: u8) -> Vec<u8> {
+    let pt = property_table_address(m, v, o);
+    let hs = m[pt] as usize;
+    let mut pa = pt + 1 + (hs * 2);
+    let mut r = Vec::new();
+    while r.is_empty() {
+        match v {
+            // V1-3:
+            //  * Size/number encoded in a single byte
+            1 | 2 | 3 => {
+                let sb = m[pa];
+                let n = sb & 0x1F;
+                let s = (sb as usize / 32) + 1;
+                if n == p {
+                    for i in 0..s {
+                        r.push(m[pa + 1 + i])
+                    }
+                } else if n < p {
+                    r = default_property(m, p);
+                } else {
+                    pa = pa + 1 + s;
+                }
+            },
+            // V4+:
+            //  * Size/number encoded in a single byte -or-
+            //  * Number in first byte, size in second
+            4 | 5 | 6 | 7 | 8 => {
+                let mut pd = pa + 1;
+                let sb = m[pa];
+                let n = sb & 0x3F;
+                let s = {
+                    if sb & 0x80 == 0x80 {
+                        pd = pd + 1;
+                        m[pa + 1] as usize & 0x3F
+                    } else {
+                        if sb & 0x40 == 0x40 {
+                            2
+                        } else {
+                            1
+                        }
+                    }
+                };
+
+                if n == p {
+                    for i in 0..s {
+                        r.push(m[pd + i]);
+                    }
+                } else if n < p {
+                    r = default_property(m, p);
+                } else {
+                    pa = pd + s;
+                }
+            },
+            _ => {}
         }
     }
-}
-
-pub struct ObjectTable {
-    version: u8,
-    property_defaults: Vec<u16>,
-    objects: Vec<Object>
-}
-
-impl fmt::Display for ObjectTable {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        writeln!(f, "Default properties:")?;
-        for i in 0..self.property_defaults.len() {
-            writeln!(f, "\t{}: ${:x}", i + 1, self.property_defaults[i])?;
-        }
-        writeln!(f, "Objects:")?;
-        for i in 0..self.objects.len() {
-            writeln!(f, "\t{}\n{}", i + 1, self.objects[i])?;
-        }
-
-        Ok(())
-    }
-}
-
-fn word_value(v: &Vec<u8>, a: usize) -> u16 {
-    let hb: u16 = (((v[a] as u16) << 8) as u16 & 0xFF00) as u16;
-    let lb: u16 = (v[a + 1] & 0xFF) as u16;
-    hb + lb
-}
-
-impl ObjectTable {
-    pub fn from_addr(m: &Vec<u8>, a: usize) -> ObjectTable {
-        let v = m[0];
-        let mut prop_def = Vec::new();
-        let prop_cnt = match v {
-            1 | 2 | 3 => 32,
-            _ => 64
-        };
-
-        for i in 1..prop_cnt {
-            prop_def.push(word_value(m, a + (i * 2)))
-        }
-
-        let mut o = Vec::new();
-        for i in 0..5 {
-            o.push(Object::from_addr(m, v, a + (i * 9) + ((prop_cnt-1) * 2)));
-        }
-        ObjectTable {
-            version: v,
-            property_defaults: prop_def,
-            objects: o
-        }
-    }
+    r
 }
