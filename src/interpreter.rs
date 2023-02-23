8de376005d2e43af2537bf3c66c0ab48a7d1e66b
Evan Day <evan.day@gmail.com>
2023-02-22 20:06:26 -0800

    Add show_status instruction; change read return type

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -12,12 +12,13 @@ pub trait Interpreter {
     fn output_stream(&mut self, stream: i16, table: usize);
     fn print(&mut self, text: String);
     fn print_table(&mut self, text: String, width: u16, height: u16, skip: u16);
-    fn read(&mut self, length: u8, time: u16) -> String;
+    fn read(&mut self, length: u8, time: u16) -> Vec<char>;
     fn read_char(&mut self, time: u16) -> char;
     fn set_colour(&mut self, foreground: u16, background: u16);
     fn set_cursor(&mut self, line: u16, column: u16);
     fn set_text_style(&mut self, style: u16);
     fn set_window(&mut self, window: u16);
+    fn show_status(&mut self, location: &str, status: &str);
     fn sound_effect(&mut self, number: u16, effect: u16, volume: u8, repeats: u8);
     fn split_window(&mut self, lines: u16);
 }
