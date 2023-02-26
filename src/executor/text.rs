74ee747750f9bb6d54bb1369146e1dafed1944e8
Evan Day <evan.day@gmail.com>
2023-02-26 11:56:19 -0800

    Arrow keys are not valid READ input

--- src/executor/text.rs
+++ src/executor/text.rs
@@ -18,7 +18,7 @@ const ALPHABET_V3: [[char; 26]; 3] = [
 pub fn valid_input(c: char) -> bool {
     if c as u8 >= 32 && c as u8 <= 126 {
         true
-    } else if c as u8 >= 129 && c as u8 <= 254 {
+    } else if c as u8 >= 145 && c as u8 <= 254 {
         true
     } else {
         false
