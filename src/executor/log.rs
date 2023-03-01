0c76adad79dd27ad0facde1f865f1043091167ca
Evan Day <evan.day@gmail.com>
2023-02-28 17:36:28 -0800

    Remove unused stuff

--- src/executor/log.rs
+++ src/executor/log.rs
@@ -1,18 +1,6 @@
 extern crate simplelog;
 
-use std::fs::File;
-
-use simplelog::*;
-
 pub fn init() {
     log4rs::init_file("log4rs.yml", Default::default()).unwrap();
-
-    // WriteLogger::init(
-    //     LevelFilter::Trace,
-    //     Config::default(),
-    //     File::create("trace.log").unwrap(),
-    // )
-    // .unwrap();
-
     trace!("Log start");
 }
