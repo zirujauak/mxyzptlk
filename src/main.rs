69f6107c405294bf119f106b0198a4ea3a1558fd
Evan Day <evan.day@gmail.com>
2023-06-17 16:56:44 -0700

    Logging

--- src/main.rs
+++ src/main.rs
@@ -23,11 +23,13 @@ fn main() {
     let name: Vec<&str> = filename.split(".").collect();
     let name = name[0].to_string();
     log4rs::init_file("log4rs.yml", Default::default()).unwrap();
-    trace!(target: "app::trace", "Start trace log for '{}'", name);
     info!(target: "app::frame", "Start frame log for '{}'", name);
+    info!(target: "app::input", "Start input log for '{}'", name);
     info!(target: "app::instruction", "Start instruction log for '{}'", name);
     info!(target: "app::memory", "Start memory log for '{}'", name);
+    info!(target: "app::quetzal", "Start quetzal log for '{}'", name);
     info!(target: "app::stack", "Start stack log for '{}'", name);
+    info!(target: "app::trace", "Start trace log for '{}'", name);
     info!(target: "app::variable", "Start variable log for '{}'", name);
     log_mdc::insert("instruction_count", format!("{:8x}", 0));
 
