db9bc2c6983c8d2773544cf36a16de47a796977c
Evan Day <evan.day@gmail.com>
2023-07-03 22:13:06 -0700

    Find the target for llvm-cov

--- build/report.sh
+++ build/report.sh
@@ -1,10 +1,13 @@
 #!/bin/zsh
+target=$(find target/debug/deps -regex ".*/mxyzptlk-[^.]*")
 
 cargo clean
-RUSTFLAGS="-C instrument-coverage" cargo --quiet test --tests
+RUSTFLAGS="-C instrument-coverage" cargo --quiet test --no-default-features --tests
 xcrun llvm-profdata merge -sparse default_*.profraw -o json5format.profdata
 xcrun llvm-cov report --use-color --ignore-filename-regex='/.cargo/registry' \
     --instr-profile=json5format.profdata \
-    --object target/debug/deps/mxyzptlk-f9babb35b31800c3 \
-    --show-instantiation-summary \
+    --object ${target} \
     --Xdemangler=rustfilt | less -R
+
+rm *.profraw
+rm json5format.profdata
