21f1ccff303c967fcb178314a18c4af123080de5
Evan Day <evan.day@gmail.com>
2023-07-05 16:10:09 -0700

    Find target _after_ code is built and tested

--- build/report.sh
+++ build/report.sh
@@ -1,13 +1,17 @@
 #!/bin/zsh
-target=$(find target/debug/deps -regex ".*/mxyzptlk-[^.]*")
 
+## Build and run tests with instrumentation
 cargo clean
 RUSTFLAGS="-C instrument-coverage" cargo --quiet test --no-default-features --tests
+target=$(find target/debug/deps -regex ".*/mxyzptlk-[^.]*")
+
+## Generate and view report
 xcrun llvm-profdata merge -sparse default_*.profraw -o json5format.profdata
 xcrun llvm-cov report --use-color --ignore-filename-regex='/.cargo/registry' \
     --instr-profile=json5format.profdata \
     --object ${target} \
     --Xdemangler=rustfilt | less -R
 
+## Clean up profile data
 rm *.profraw
 rm json5format.profdata
