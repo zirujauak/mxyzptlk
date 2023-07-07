d2cbdd8b11000c154ad94ff46118688e00e93b9d
Evan Day <evan.day@gmail.com>
2023-07-06 17:09:10 -0700

    Single-thread tests

--- build/profile.sh
+++ build/profile.sh
@@ -2,7 +2,7 @@
 
 ## Build and run tests with instrumentation
 cargo clean
-RUSTFLAGS="-C instrument-coverage" cargo --quiet test --no-default-features --tests
+RUSTFLAGS="-C instrument-coverage" cargo --quiet test --no-default-features --tests -- --test-threads=1
 
 ## Find the target binary
 target=$(find target/debug/deps -regex ".*/mxyzptlk-[^.]*")
